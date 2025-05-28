f6789b87bdfb7ab3aefdba1371f4224fc1d53b7c
SariWolfheart <kaylanewcomb7@gmail.com>
2025-05-28 14:40:12 -0400

    refactored code, moved word counting function to stats.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_number_of_words
+
 def get_book_text(file_path):
     """
     Reads the content of a book file and returns it as a string.
@@ -10,19 +12,7 @@ def get_book_text(file_path):
     """
     with open(file_path) as file:
         return file.read()
-    
-def get_number_of_words(text):
-    """
-    Counts the number of words in a given text.
-    
-    Args:
-        text (str): The text to count words in.
         
-    Returns:
-        int: The number of words in the text.
-    """
-    return len(text.split())
-    
 def main():
     """
     Reads the text of 'frankenstein.txt', counts the number of words in the document,
